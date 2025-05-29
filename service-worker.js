29ccc3a1476bd809c97957ec66238c8461b45fee
lpseccp <lpseccp@gmail.com>
2025-05-29 20:51:11 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,26 @@
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('finance-app-v2').then(cache => {
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('finance-app').then((cache) => {
             return cache.addAll([
-                './',
-                './index.html',
-                './style.css',
-                './manifest.json',
-                './balanco.html',
-                './dre.html',
-                './liquidez.html',
-                './estrutura.html',
-                './app.js',
-                './icon.png'
+                '/',
+                '/index.html',
+                '/balanco.html',
+                '/dre.html',
+                '/liquidez.html',
+                '/estrutura.html',
+                '/style.css',
+                '/app.js',
+                '/manifest.json',
+                '/icon.png'
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
         })
     );
 });
